96d0bb45b9d32095d131dc3ab487b873b88e937e
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-11-27 16:14:55 +0200

    adding an example code

--- streamingContent.js
+++ streamingContent.js
@@ -1,17 +1,24 @@
 var fs = require('fs');
+var time = process.argv[2];
 const readable = fs.createReadStream('data.txt');
-// var data = process.argv[1];
-// data="";
-readable.on('data', (chunk)=>{
-  // console.log(chunk);
-  process.stdout.write(chunk);
-  // data += chunk;
-});
-// readable.on('end', ()=>{
-//   process.stdout.write(data);
-// })
-
-// fs.readFile('data.txt', 'utf8', function(err, data) {
-//     if (err) throw err;
-//     console.log(data);
-// });
+if (time) {
+  time = Number(time.split('s')[0]);
+  if (time) {
+    time = time * 1000;
+    readable.on('data', (chunk) => {
+      readable.pause();
+      // process.stdout.write(chunk);
+      console.log(chunk);
+      setTimeout(() => {
+        readable.resume();
+      }, time);
+    });
+  }else{
+    console.log("time is not a Number");
+  }
+} else {
+  readable.on('data', (chunk) => {
+    process.stdout.write(chunk);
+    process.stdout.write("-----------------------------------------");
+  })
+}
